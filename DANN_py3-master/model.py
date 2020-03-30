8b06c32db5c470bf3ac94dca631bef6bd6cb7a78
Jinsung Jeon <jjsjjs0902@naver.com>
2020-03-30 23:07:21 +0900

    First commit

--- DANN_py3-master/model.py
+++ DANN_py3-master/model.py
@@ -36,7 +36,6 @@ class CNNModel(nn.Module):
         self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
 
     def forward(self, input_data, alpha):
-        input_data = input_data.expand(input_data.data.shape[0], 3, 32, 32)
         feature = self.feature(input_data)
         feature = feature.view(-1, 50 * 4 * 4)
         reverse_feature = ReverseLayerF.apply(feature, alpha)
