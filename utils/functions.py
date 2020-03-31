5d4130fadda6ceef35c712dd10e66c35eb028b33
Jinsung Jeon <jjsjjs0902@naver.com>
2020-03-31 17:48:57 +0900

    First commit

--- utils/functions.py
+++ utils/functions.py
@@ -4,12 +4,11 @@ from torch.autograd import Function
 class ReverseLayerF(Function):
 
     @staticmethod
-    def forward(x):
+    def forward(ctx, x):
         return x.view_as(x)
 
     @staticmethod
-    def backward(grad_output):
+    def backward(ctx, grad_output):
         output = grad_output.neg()
         return output
-
 
