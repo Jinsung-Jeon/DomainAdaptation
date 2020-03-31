6968470410dc4f09b41839c6156e9c1b01771f83
Jinsung Jeon <jjsjjs0902@naver.com>
2020-03-31 14:35:25 +0900

    First commit

--- total.sh
+++ total.sh
@@ -1,12 +1,1 @@
-cd DANN_py3-master
-python main.py
-
-cd ..
-CUDA_VISIBLE_DEVICES=0,1 python main.py --method self-supervision --source cifar10 --nepoch 35 --milestone_1 20 --milestone_2 30  --batch_size 100 --width 8 --target stl10 --flip --outf output/cifar10
-
-cd PixDA
-python main.py
-
-cd ..
-cd python-adda-master
-python main.py
+CUDA_VISIBLE_DEVICES=1 python main.py --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 100 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cifar_stl_r'
