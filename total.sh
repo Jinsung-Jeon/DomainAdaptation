8a98ae75467cde48e30c750cac4081db30a16595
Jinsung Jeon <jjsjjs0902@naver.com>
2020-03-31 18:19:31 +0900

    First commit

--- total.sh
+++ total.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,2 python main.py --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 100 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cifar_stl_r_2'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 100 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cifar_stl_r_2'
