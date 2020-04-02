e7019c50de002c3e1e0aa3ab6bd5ad4910bb3911
Jinsung Jeon <jjsjjs0902@naver.com>
2020-04-02 11:41:45 +0900

    First commit

--- total.sh
+++ total.sh
@@ -1,2 +1,5 @@
-CUiDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cifar_stl_AD'
-CUiDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --flip --outf 'output/mnist_svhn_AD'
+#CUiDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cifar_stl_AD'
+#CUiDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --flip --outf 'output/mnist_svhn_AD'
+
+CUiDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cifar_stl_AD_ADAM'
+CUiDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --flip --outf 'output/mnist_svhn_AD_ADAM'
