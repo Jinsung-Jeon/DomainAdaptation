369ba177a91cbb32a8adfcc7b6eff42ddc13c73b
Jinsung Jeon <jjsjjs0902@naver.com>
2020-04-04 23:09:19 +0900

    First commit

--- total.sh
+++ total.sh
@@ -1,3 +1,6 @@
 #CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cifar_stl_AD'
 #CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 200 --lr 0.01 --milestone_1 50 --milestone_2 75 --flip --outf 'output/mnist_svhn_AD'
 
+# soft label knowledge distillation
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 100 --lr 0.01 --milestone_1 50 --milestone_2 75 --outf 'output/cs_kd'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source mnist --target svhn_extra --num_batches_per_test 10000 --nepoch 100 --lr 0.01 --milestone_1 50 --milestone_2 75 --outf 'output/ms_kd'
