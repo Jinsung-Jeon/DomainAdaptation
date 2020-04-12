941a381158842370dc7a26292f6b91811670e5f4
Jinsung Jeon <jjsjjs0902@naver.com>
2020-04-12 23:32:01 +0900

    First commit

--- main.py
+++ main.py
@@ -111,9 +111,8 @@ for epoch in range(1, args.nepoch+1):
     epoch_stats = train(args, net, ext, sstasks, 
         criterion, criterion_d, optimizer, scheduler, sc_tr_loader, sc_te_loader, tg_tr_loader, tg_te_loader)
     #all_epoch_stats.append(epoch_stats)
-    torch.save(all_epoch_stats, args.outf + '/loss.pth')
     #plot_all_epoch_stats(all_epoch_stats, args.outf)
-    if args.method == 'self-supervision_Adap':
+    if args.method == 'self-supervision_Adapt':
         excerpt, pseudo_labels, input_z = labeling(args, net, tg_tr_loader)
         epoch_stats = train_d(args, net, ext, sstasks, criterion, optimizer, sc_tr_loader, sc_tr_dataset, sc_te_loader, tg_te_dataset, tg_te_loader, excerpt, pseudo_labels, input_z)
         all_epoch_stats.append(epoch_stats)
--- total.sh
+++ total.sh
@@ -20,4 +20,4 @@
 # self-supervised and pseudo_label
 
 #CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cs_SS_label'
-CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source mnist --target svhn --num_batches_per_test 10000 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS_label'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source mnist --target svhn --num_batches_per_test 10000 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS_label'
