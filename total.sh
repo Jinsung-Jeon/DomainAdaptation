cb27ecc5b109e5e5784a1ff9577038a2c85c7138
Jinsung Jeon <jjsjjs0902@naver.com>
2020-04-14 21:57:17 +0900

    First commit

--- total.sh
+++ total.sh
@@ -23,11 +23,11 @@
 #CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source mnist --target svhn --num_batches_per_test 10000 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS_label'
 
 # 비교 실험
-CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source mnist --target svhn --num_batches_per_test 10000 --threshold 0.7 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS'
-CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source mnist --target svhn --num_batches_per_test 10000 --threshold 0.7 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS_adapt'
-CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --threshold 0.7 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cs_SS'
-CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --threshold 0.7 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cs_SS_adapt'
-CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source mnist --target svhn --num_batches_per_test 10000 --threshold 0.5 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS_adapt_5'
-CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --threshold 0.5 --nepoch 100 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cs_SS_adapt_5'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source mnist --target svhn --num_batches_per_test 10000 --threshold 0.7 --nepoch 50 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source mnist --target svhn --num_batches_per_test 10000 --threshold 0.7 --nepoch 50 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS_adapt'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --threshold 0.7 --nepoch 50 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cs_SS'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --threshold 0.7 --nepoch 50 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cs_SS_adapt'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source mnist --target svhn --num_batches_per_test 10000 --threshold 0.5 --nepoch 50 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/ms_SS_adapt_5'
+CUDA_VISIBLE_DEVICES=0,1,2 python main.py --batch_size 256 --method self-supervision_Adapt --width 16 --source cifar10 --target stl10 --num_batches_per_test 10000 --threshold 0.5 --nepoch 50 --lr 0.1 --milestone_1 50 --milestone_2 75 --rotation --outf 'output/cs_SS_adapt_5'
 
 # graph나 sharp를 써보는건 어떨까...
