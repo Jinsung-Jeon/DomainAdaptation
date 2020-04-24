802ad122bdc949ab709f682ca59984d83c428a96
Jinsung Jeon <jjsjjs0902@naver.com>
2020-04-24 23:57:20 +0900

    First commit

--- pytorch-adda-master/main.py
+++ pytorch-adda-master/main.py
@@ -2,7 +2,7 @@
 
 import params
 from core import eval_src, eval_tgt, train_src, train_tgt
-from models import Discriminator, LeNetClassifier, LeNetEncoder, ResNetEncoder
+from models import Discriminator, LeNetClassifier, LeNetEncoder, ResNetEncoder, Discriminator_s
 from utils import get_data_loader, init_model, init_random_seed, get_dataset
 import os
 import torch.nn as nn
